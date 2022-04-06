447993296b7ba9cf3932425dae0499f947dfb5a2
WX1999 <46165993+wong00@users.noreply.github.com>
2022-04-06 14:08:46 +0800

    Update backwarp.py

--- backwarp.py
+++ backwarp.py
@@ -1,1 +1,34 @@
+def optical_flow_warping(x, flo, pad_mode="zeros"):
+    """
+    warp an image/tensor (im2) back to im1, according to the optical flow
 
+    x: [B, C, H, W] (im2)
+    flo: [B, 2, H, W] flow
+    pad_mode (optional): ref to https://pytorch.org/docs/stable/nn.functional.html#grid-sample
+        "zeros": use 0 for out-of-bound grid locations,
+        "border": use border values for out-of-bound grid locations
+    """
+    B, C, H, W = x.size()
+    # mesh grid
+    xx = torch.arange(0, W).view(1, -1).repeat(H, 1)
+    yy = torch.arange(0, H).view(-1, 1).repeat(1, W)
+    xx = xx.view(1, 1, H, W).repeat(B, 1, 1, 1)
+    yy = yy.view(1, 1, H, W).repeat(B, 1, 1, 1)
+    grid = torch.cat((xx, yy), 1).float()
+
+    vgrid = grid + flo  # warp后，新图每个像素对应原图的位置
+
+    # scale grid to [-1,1]
+    vgrid[:, 0, :, :] = 2.0 * vgrid[:, 0, :, :].clone() / max(W - 1, 1) - 1.0
+    vgrid[:, 1, :, :] = 2.0 * vgrid[:, 1, :, :].clone() / max(H - 1, 1) - 1.0
+
+    vgrid = vgrid.permute(0, 2, 3, 1)
+    output = F.grid_sample(x, vgrid, padding_mode=pad_mode)
+
+    mask = torch.ones(x.size())
+    mask = F.grid_sample(mask, vgrid)
+
+    mask[mask < 0.9999] = 0
+    mask[mask > 0] = 1
+
+    return output * mask
